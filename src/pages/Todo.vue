bf490ddd7f7c76b09181339010e49d274de88ad0
José Luis González <joseluisgonsan@gmail.com>
2022-07-31 13:00:54 +0200

    Delete Task

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -8,16 +8,33 @@
       <q-item
         v-for="task in tasks"
         :key="task.id"
+        clickable
+        @click="clickTask(task)"
         v-ripple
+        :class="{ 'done bg-blue-1': task.done }"
       >
         <q-item-section avatar>
           <q-checkbox
             v-model="task.done"
             color="primary"
+            class="no-pointer-events"
           />
         </q-item-section>
         <q-item-section>
           <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+        <q-item-section
+          v-if="task.done"
+          side
+        >
+          <q-btn
+            flat
+            round
+            color="primary"
+            icon="delete"
+            dense
+            @click.stop="deleteTask(task)"
+          />
         </q-item-section>
       </q-item>
     </q-list>
@@ -34,9 +51,28 @@
     setup() {
       const taskStore = TaskStore()
 
+      const clickTask = (task) => {
+        taskStore.toggleTask(task)
+      }
+
+      const deleteTask = (task) => {
+        taskStore.deleteTask(task)
+      }
+
       return {
         tasks: taskStore.getTasks,
+        clickTask,
+        deleteTask,
       }
     },
   })
 </script>
+
+<style lang="scss" scoped>
+  .done {
+    .q-item__label {
+      text-decoration: line-through;
+      color: #bbb;
+    }
+  }
+</style>
--- src/stores/tasks.js
+++ src/stores/tasks.js
@@ -13,10 +13,22 @@ const TaskStore = defineStore({
   getters: {
     getTaskById: (state) => (id) => state.tasks.find((task) => task.id === id),
     // get task order by created at dec asc cambia el -1 por 1 y viceversa
-    getTasks: (state) =>
-      state.tasks.sort((a, b) => (a.createdAt < b.createdAt) ? -1 : 1),
+    getTasks: (state) => state.tasks.sort((a, b) => (a.createdAt < b.createdAt ? -1 : 1)),
   },
-  actions: {},
+  actions: {
+    toggleTask(task) {
+      console.log('toggleTask', task)
+      task.done = !task.done
+    },
+
+    deleteTask(task) {
+      console.log('deleteTask', task)
+      const index = this.tasks.indexOf(task)
+      this.tasks.splice(index, 1)
+      // tambien lo puedo hacer como
+      // this.state.tasks = this.state.tasks.filter((t) => t.id !== task.id)
+    },
+  },
 })
 
 export default TaskStore
